6f0c4d09f9d54f2243f6035278ec23aaf8e67036
A Wakabayashi in Tokyo <wakabayashi@entfaltung.space>
2021-05-08 17:19:51 +0900

    Rossor attractor

---
+++ attractor_orbit_001.f90
@@ -0,0 +1,81 @@
+program attractor_orbit_001
+
+        implicit none
+        integer :: steps
+        double precision :: x = 1d0, y=1d0, z=1d0
+        double precision :: sx, sy, sz
+        double precision :: xdot, ydot, zdot
+        double precision :: kx1, ky1, kz1
+        double precision :: kx2, ky2, kz2
+        double precision :: kx3, ky3, kz3
+        double precision :: kx4, ky4, kz4
+        double precision,parameter :: a=0.344d0, b=0.4d0, c=4.5d0
+        double precision,parameter :: dt = 1d-4
+        double precision,parameter :: total_time = 200.0
+        integer,parameter :: nb_max_steps = int(total_time / dt)
+        integer,parameter :: output_interval = int(0.03 / dt)
+
+        open(12, file="attractor_orbit_001.xyz", status="replace")
+
+        do steps = 1, nb_max_steps
+
+         call attractor(x,y,z)
+
+         kx1 = xdot
+         ky1 = ydot
+         kz1 = zdot
+
+         sx = x + kx1 * 0.5d0 * dt
+         sy = y + ky1 * 0.5d0 * dt
+         sz = z + kz1 * 0.5d0 * dt
+
+         call attractor(sx,sy,sz)
+
+         kx2 = xdot
+         ky2 = ydot
+         kz2 = zdot
+
+         sx = x + kx2 * 0.5d0 * dt
+         sy = y + ky2 * 0.5d0 * dt
+         sz = z + kz2 * 0.5d0 * dt
+        
+         call attractor(sx,sy,sz)
+
+         kx3 = xdot
+         ky3 = ydot
+         kz3 = zdot
+
+         sx = x + kx3 * dt
+         sy = y + ky3 * dt
+         sz = z + kz3 * dt
+
+         call attractor(sx,sy,sz)
+
+         kx4 = xdot
+         ky4 = ydot
+         kz4 = zdot
+
+         x = x + (kx1 + 2.0d0 * kx2 + 2.0d0 * kx3 + kx4)/6d0 * dt
+         y = y + (ky1 + 2.0d0 * ky2 + 2.0d0 * ky3 + ky4)/6d0 * dt
+         z = z + (kz1 + 2.0d0 * kz2 + 2.0d0 * kz3 + kz4)/6d0 * dt
+
+         if(mod(steps,output_interval) .eq. 0) write(12,'(3f9.4)') x, y, z
+
+        end do
+
+        close(12)
+
+        contains
+
+        subroutine attractor(x,y,z)
+
+                implicit none
+                double precision :: x, y, z
+
+                xdot = - y- z
+                ydot = x + a * y
+                zdot = b * x - (c-x) * z
+
+        end subroutine
+
+end program
---
+++ attractor_orbit_001.plt
@@ -0,0 +1,3 @@
+set key title sprintf("a = 0.344, b = 0.4, c = 0.2")
+splot "attractor_orbit_001.xyz" using 1:2:3 with line notitle
+pause mouse
